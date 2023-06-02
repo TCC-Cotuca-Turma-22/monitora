758ca850d07d9d1a164b74150cca971bbe6c2959
u22597 <cc22597@g.unicamp.br>
2023-06-01 21:11:16 -0300

    Ajustes de tela

--- app/src/main/java/com/teconsis/monitora/ConfiguracoesActivity.kt
+++ app/src/main/java/com/teconsis/monitora/ConfiguracoesActivity.kt
@@ -1,33 +1,24 @@
 package com.teconsis.monitora
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
-class ConfiguracaoActivity : AppCompatActivity() {
-
-    private lateinit var aparelhoEditText: EditText
-    private lateinit var temporizadorEditText: EditText
+class ConfiguracoesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configuracoes)
 
-        aparelhoEditText = findViewById(R.id.aparelhoEditText)
-        temporizadorEditText = findViewById(R.id.temporizadorEditText)
         val gravarButton: Button = findViewById(R.id.gravarButton)
 
         gravarButton.setOnClickListener {
-            val aparelho = aparelhoEditText.text.toString()
-            val temporizador = temporizadorEditText.text.toString()
-
-            // Lógica para gravar os dados de aparelho e temporizador na tabela DISPOSITIVOS
-            // e adicioná-los à lista de dados
-
-            // Limpar os campos de texto após a gravação
-            aparelhoEditText.text.clear()
-            temporizadorEditText.text.clear()
+            // Lógica para gravar os dados do usuário na tabela de usuários
+            // após a gravação, retorne para a tela de login (MainActivity)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Encerra a atividade atual (CadastroUsuarioActivity)
         }
     }
 }
