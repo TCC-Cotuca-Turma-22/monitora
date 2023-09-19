6e9ecb0dbef3c01c9370252815f3df95cefd295e
Daniel Freitas de Oliveira <114271103+dnieloliveira@users.noreply.github.com>
2023-09-19 19:04:02 -0300

    Daniel (#11)
    Criar classe aparelho

---
+++ app/src/main/java/com/teconsis/monitora/Aparelho.kt
@@ -0,0 +1,7 @@
+package com.teconsis.monitora
+
+data class Aparelho(
+    val ID_APARELHO: Long,
+    val CODIGO_INFRA: String?,
+    val DESCRICAO: String?
+)
--- app/src/main/java/com/teconsis/monitora/ConfigurarActivity.kt
+++ app/src/main/java/com/teconsis/monitora/ConfigurarActivity.kt
@@ -2,6 +2,7 @@ package com.teconsis.monitora
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
