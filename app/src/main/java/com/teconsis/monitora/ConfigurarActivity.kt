cad822c777f890ff2e810c266d06ec8b4a80f396
amalemida <amalemida@gmail.com>
2023-10-04 22:49:29 -0300

    Ajustar ConfigurarActivity

--- app/src/main/java/com/teconsis/monitora/ConfigurarActivity.kt
+++ app/src/main/java/com/teconsis/monitora/ConfigurarActivity.kt
@@ -1,36 +1,67 @@
 package com.teconsis.monitora
 
+import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
 class ConfigurarActivity : AppCompatActivity() {
 
     private lateinit var aparelhoEditText: EditText
     private lateinit var temporizadorEditText: EditText
+    private lateinit var databaseHelper: DatabaseHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configurar)
 
-        aparelhoEditText = findViewById(R.id.aparelhoEditText)
+        databaseHelper = DatabaseHelper(this)
+        val sharedPreferences = getSharedPreferences("mySharedPreferences", Context.MODE_PRIVATE)
+        val loggedInUserId = sharedPreferences.getLong("loggedInUserId", -1)
+       // aparelhoEditText = findViewById(R.id.aparelhoEditText)
         temporizadorEditText = findViewById(R.id.temporizadorEditText)
         val gravarButton: Button = findViewById(R.id.gravarButton)
         val retornarButton: Button = findViewById(R.id.retornarButton)
 
+        val aparelhoSpinner: Spinner = findViewById(R.id.aparelhoSpinner)
+        val aparelhoList = databaseHelper.getAllAparelhos()
+        val aparelhoAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            aparelhoList.map { it.descricao })
+        aparelhoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        aparelhoSpinner.adapter = aparelhoAdapter
+
         gravarButton.setOnClickListener {
-            val aparelho = aparelhoEditText.text.toString()
-            val temporizador = temporizadorEditText.text.toString()
+            val temporizador = temporizadorEditText.text.toString().toIntOrNull() ?: 0
 
-            // Lógica para gravar os dados de aparelho e temporizador na tabela DISPOSITIVOS
-            // e adicioná-los à lista de dados
-
-            // Limpar os campos de texto após a gravação
-            aparelhoEditText.text.clear()
             temporizadorEditText.text.clear()
+
+            val db = DatabaseHelper(this).writableDatabase
+            val values = ContentValues()
+
+            values.put(DatabaseHelper.COLUMN_ID_USUARIO, loggedInUserId)
+            values.put(DatabaseHelper.COLUMN_ID_DISPOSITIVO, 1)
+            values.put(DatabaseHelper.COLUMN_ID_MODO_OPERACAO, 1)
+            values.put(DatabaseHelper.COLUMN_TEMPORIZADOR, temporizador)
+
+            val configuracaoId = db.insert(DatabaseHelper.TABLE_CONFIGURACAO_USUARIO, null, values)
+
+// Verifique se a inserção foi bem-sucedida
+            if (configuracaoId != -1L) {
+                // Inserção bem-sucedida
+            } else {
+                // Falha na inserção
+            }
+
         }
+
+
 
         retornarButton.setOnClickListener {
             val intent = Intent(this, ConfiguracoesActivity::class.java)
