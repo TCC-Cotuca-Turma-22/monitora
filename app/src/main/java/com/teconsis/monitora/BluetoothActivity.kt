f91e294eca008d749722f1d94a4995ae6bfe35c7
Wanderley Paranhos <72552437+wsparanhos@users.noreply.github.com>
2023-11-10 18:35:13 -0300

    Ajustes Comunicação Bluetotth 2023_04_11

--- app/src/main/java/com/teconsis/monitora/BluetoothActivity.kt
+++ app/src/main/java/com/teconsis/monitora/BluetoothActivity.kt
@@ -10,143 +10,144 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ListView
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import java.io.IOException
 import java.util.UUID
 
 class BluetoothActivity : AppCompatActivity() {
 
-    private var bluetoothAdapter: BluetoothAdapter? = null
-    private val discoveredDevices = ArrayList<BluetoothDevice>()
-    private var deviceList: ListView? = null
-    private lateinit var scanButton: Button
-    private lateinit var messageInput: EditText
-    private lateinit var sendButton: Button
-    private lateinit var messageOutput: TextView
+    private val bluetoothAdapter: BluetoothAdapter? by lazy { BluetoothAdapter.getDefaultAdapter() }
+    private val messageInput: EditText by lazy { findViewById(R.id.messageInput) }
+    private val sendButton: Button by lazy { findViewById(R.id.sendButton) }
+    private val messageOutput: TextView by lazy { findViewById(R.id.messageOutput) }
+    private val connectionStatusIcon: ImageView by lazy { findViewById(R.id.connectionStatusIcon) }
+    private val connectionStatusText: TextView by lazy { findViewById(R.id.connectionStatusText) }
+    private val backButton: Button by lazy { findViewById(R.id.getBackButton) }
+    private val pairedDeviceName: TextView by lazy { findViewById(R.id.pairedDeviceName) }
 
-    private val bluetoothReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            val action = intent?.action
-            if (BluetoothDevice.ACTION_FOUND == action) {
-                val device: BluetoothDevice? = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-                if (device != null) {
-                    discoveredDevices.add(device)
-                }
-                val deviceNames = discoveredDevices.map { it.name ?: "Unnamed" }.toTypedArray()
-                deviceList?.adapter = ArrayAdapter(this@BluetoothActivity, android.R.layout.simple_list_item_1, deviceNames)
-            }
-        }
-    }
-
-    private val hc05DeviceName = "HC-05" // Nome do dispositivo HC-05
-    private val hc05UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB") // Exemplo de UUID genérico
+    private val hc05DeviceName = "HC-05"
+    private val hc05UUID = UUID.fromString("0000FFE0-0000-1000-8000-00805F9B34FB")
     private var bluetoothSocket: BluetoothSocket? = null
 
     companion object {
         private const val REQUEST_ENABLE_BT = 1
         private const val REQUEST_BLUETOOTH_SCAN = 2
-        private const val REQUEST_BLUETOOTH_CONNECT = 3 // Nova constante para solicitar permissão BLUETOOTH_CONNECT
+        private const val REQUEST_BLUETOOTH_CONNECT = 3
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bluetooth)
 
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-        if (bluetoothAdapter == null) {
-            showBluetoothUnavailableDialog()
-        } else if (!bluetoothAdapter?.isEnabled!!) {
-            requestBluetoothEnable()
-        }
-
-        deviceList = findViewById(R.id.deviceList)
-        scanButton = findViewById(R.id.scanButton)
-        messageInput = findViewById(R.id.messageInput)
-        sendButton = findViewById(R.id.sendButton)
-        messageOutput = findViewById(R.id.messageOutput)
-
-        val permission = Manifest.permission.BLUETOOTH_ADMIN
-        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(permission), REQUEST_ENABLE_BT)
-        }
-
-        scanButton.setOnClickListener {
-            startDiscovery()
-        }
-
-        sendButton.setOnClickListener {
-            val message = messageInput.text.toString()
-            if (bluetoothSocket != null) {
-                try {
-                    bluetoothSocket!!.outputStream.write(message.toByteArray())
-                    messageInput.text.clear()
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                    showMessage("Erro ao enviar mensagem: ${e.message}")
+        try {
+            if (bluetoothAdapter == null) {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Bluetooth Indisponível")
+                builder.setMessage("O Bluetooth não está disponível neste dispositivo.")
+                builder.setPositiveButton("OK") { dialog, _ ->
+                    dialog.dismiss()
+                    finish()
                 }
+                val dialog = builder.create()
+                dialog.show()
             } else {
-                showMessage("Dispositivo não conectado.")
+                if (!bluetoothAdapter!!.isEnabled) {
+                    requestBluetoothEnable()
+                }
+
+                backButton.setOnClickListener {
+                    finish()
+                }
+
+                sendButton.setOnClickListener {
+                    val message = messageInput.text.toString()
+                    if (bluetoothSocket != null) {
+                        try {
+                            bluetoothSocket!!.outputStream.write(message.toByteArray())
+                            messageInput.text.clear()
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                            showMessage("Erro ao enviar mensagem: ${e.message}")
+                        }
+                    } else {
+                        showMessage("Dispositivo não conectado.")
+                    }
+                }
+
+                connectToHC05()
             }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            showMessage("Erro inesperado: ${e.message}")
         }
-
-        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
-        registerReceiver(bluetoothReceiver, filter)
-
-        connectToHC05()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        unregisterReceiver(bluetoothReceiver)
-        closeBluetoothSocket()
+        try {
+            closeBluetoothSocket()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            showMessage("Erro ao encerrar: ${e.message}")
+        }
     }
 
-    private fun startDiscovery() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BLUETOOTH_SCAN), REQUEST_BLUETOOTH_SCAN)
-            return
+    private fun requestBluetoothEnable() {
+        val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        try {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.BLUETOOTH_CONNECT
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BLUETOOTH_CONNECT), REQUEST_ENABLE_BT)
+            } else {
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            showMessage("Erro ao solicitar ativação do Bluetooth: ${e.message}")
         }
-        if (bluetoothAdapter?.isDiscovering == true) {
-            bluetoothAdapter!!.cancelDiscovery()
-        }
-        discoveredDevices.clear()
-        deviceList?.adapter = null // Limpa a lista de dispositivos antes de iniciar uma nova descoberta
-        bluetoothAdapter?.startDiscovery()
     }
 
     private fun connectToHC05() {
-        val pairedDevices = bluetoothAdapter?.bondedDevices
-        if (pairedDevices != null) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                // Solicitar permissão BLUETOOTH_CONNECT
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BLUETOOTH_CONNECT), REQUEST_BLUETOOTH_CONNECT)
-            } else {
-                for (device in pairedDevices) {
-                    if (device.name == hc05DeviceName) {
-                        try {
-                            bluetoothSocket = device.createRfcommSocketToServiceRecord(hc05UUID)
-                            bluetoothSocket?.connect()
-                            // Agora você está conectado ao dispositivo HC-05
-                            showMessage("Conectado ao dispositivo: ${device.name}")
-                            // Você pode usar o bluetoothSocket para a comunicação
-                        } catch (e: Exception) {
-                            e.printStackTrace()
-                            showMessage("Erro ao conectar ao dispositivo: ${e.message}")
+        try {
+            val pairedDevices = bluetoothAdapter?.bondedDevices
+            if (pairedDevices != null) {
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.BLUETOOTH_CONNECT
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BLUETOOTH_CONNECT), REQUEST_BLUETOOTH_CONNECT)
+                } else {
+                    for (device in pairedDevices) {
+                        if (device.name == hc05DeviceName) {
+                            try {
+                                bluetoothSocket = device.createRfcommSocketToServiceRecord(hc05UUID)
+                                bluetoothSocket?.connect()
+                                showMessage("Conectado ao dispositivo: ${device.name}")
+                                pairedDeviceName.text = "Conectado a: ${device.name}"
+                            } catch (e: Exception) {
+                                e.printStackTrace()
+                                showMessage("Erro ao conectar ao dispositivo: ${e.message}")
+                            }
                         }
                     }
                 }
             }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            showMessage("Erro ao conectar ao dispositivo HC-05: ${e.message}")
         }
     }
 
@@ -155,27 +156,11 @@ class BluetoothActivity : AppCompatActivity() {
             bluetoothSocket?.close()
         } catch (e: Exception) {
             e.printStackTrace()
+            showMessage("Erro ao encerrar conexão Bluetooth: ${e.message}")
         }
     }
 
     private fun showMessage(message: String) {
         messageOutput.text = messageOutput.text.toString() + "\n" + message
-    }
-
-    private fun showBluetoothUnavailableDialog() {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Bluetooth Indisponível")
-        builder.setMessage("O Bluetooth não está disponível neste dispositivo.")
-        builder.setPositiveButton("OK") { dialog, _ ->
-            dialog.dismiss()
-            finish()
-        }
-        val dialog = builder.create()
-        dialog.show()
-    }
-
-    private fun requestBluetoothEnable() {
-        val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
     }
 }
--- app/src/main/java/com/teconsis/monitora/CadastroAparelhoActivity.kt
+++ app/src/main/java/com/teconsis/monitora/CadastroAparelhoActivity.kt
@@ -1,12 +1,12 @@
 package com.teconsis.monitora
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class CadastroAparelhoActivity : AppCompatActivity() {
 
